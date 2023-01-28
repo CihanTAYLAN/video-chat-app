63d47e16d682e7e616212d67ed51671392ca77f6
Cihan TAYLAN <cihantaylan@cihantaylan.com>
2023-01-28 22:50:53 +0300

    repo-clear

--- Frontend/src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import globalReducer from "./Slices/globalSlice";
-
-export default configureStore({
-	reducer: {
-		global: globalReducer,
-	},
-});
