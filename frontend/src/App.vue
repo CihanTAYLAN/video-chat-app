aea05deeae62f731abe8a7a32f52390b4b59c605
Cihan TAYLAN <cihantaylan@cihantaylan.com>
2023-10-21 10:56:02 +0300

    change pc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import io from "socket.io-client";
+const socket = io("localhost:8081");
+
+socket.on("connect", () => {
+	socket.emit("video-chat", "Hello!", { mr: "john" }, Uint8Array.from([1, 2, 3, 4]));
+});
+
+socket.on("video-chat", (data) => {
+	console.log(data);
+});
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>HI</div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ server/src/app/app.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class VideoGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('VideoGateway');
+
+  @SubscribeMessage('video-chat')
+  handleMessage(client: Socket, payload: string): void {
+    console.log(payload);
+    this.server.emit('video-chat', payload + '1');
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
